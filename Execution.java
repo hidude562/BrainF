df2e123a1615b5b445a3acb67a9ed24542b43b8f
Nathan M <82677882+hidude562@users.noreply.github.com>
2022-08-18 12:23:50 -0400

    Add support for custom integer length

--- Execution.java
+++ Execution.java
@@ -6,16 +6,14 @@ class Execution {
 
     public static class Memory {
         int ram_words;
-        int ram_word_width;
+        short ram_word_width;
         int pc;
         int pointer;
         short pointer_mem_contents;
         short[] ram;
         Stack<Integer> loop_stack;
 
-        // TODO - implement ram_word_width
-
-        public Memory(int ram_words, int ram_word_width) {
+        public Memory(int ram_words, short ram_word_width) {
             this.ram_words = ram_words;
             this.ram_word_width = ram_word_width;
             this.pc = 0;
@@ -53,14 +51,22 @@ class Execution {
     void get_pointer_mem_contents() {
         memory.pointer_mem_contents = memory.ram[memory.pointer];
     }
+    
+    int simulateBitWidth(int number) {
+      int fit_num = number % (memory.ram_word_width);
+      if (fit_num < 0) {
+         fit_num = fit_num + memory.ram_word_width;
+      }
+      return fit_num;
+    }
 
     void execute_instruction(byte character, boolean simulate) {
         get_pointer_mem_contents();
         switch(character) {
             case 0 -> {int start_of_loop = memory.pc; memory.loop_stack.push(start_of_loop); if(memory.pointer_mem_contents == (byte) 0) {memory.pc++; while(memory.loop_stack.search(start_of_loop) != -1) {execute_instruction(this.code[memory.pc], false);} memory.pc--;}}
             case 1 -> {int start_of_loop = memory.loop_stack.pop(); if(memory.pointer_mem_contents != (byte) 0) {memory.pc = start_of_loop; memory.loop_stack.push(start_of_loop);}}
-            case 2 -> {if(simulate)memory.ram[memory.pointer]++;} // TODO implement this to be as if it were unsigned
-            case 3 -> {if(simulate)memory.ram[memory.pointer]--;} // TODO implement this to be as if it were unsigned
+            case 2 -> {if(simulate)memory.ram[memory.pointer]++; memory.ram[memory.pointer] = (short)simulateBitWidth(memory.ram[memory.pointer]);}
+            case 3 -> {if(simulate)memory.ram[memory.pointer]--; memory.ram[memory.pointer] = (short)simulateBitWidth(memory.ram[memory.pointer]);}
             case 4 -> {if(simulate)memory.pointer--;}
             case 5 -> {if(simulate)memory.pointer++;}
             case 6 -> {if(simulate)System.out.print((char) memory.pointer_mem_contents);} // (char) memory.pointer_mem_contents
@@ -89,7 +95,7 @@ class Execution {
             }
         }
         // TODO: Implement the ram_word_width variable
-        memory = new Memory(4096, 256);
+        memory = new Memory(4096, (short)256);
     }
 }
 
